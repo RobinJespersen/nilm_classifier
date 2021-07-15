756e2f3adbcba5e383b6f9b664709055030b0e89
Robin Jespersen <RobinJespersen@users.noreply.github.com>
2021-07-15 16:55:39 +0200

    documentation

--- wfdknn_classification.py
+++ wfdknn_classification.py
@@ -80,10 +80,8 @@ class WfdknnClassifier(ClassifierMixin, BaseEstimator):
 
         Parameters
         ----------
-        X : array-like of shape (n_queries, n_features), \
-                or (n_queries, n_indexed) if metric == 'precomputed'
-            Test samples.
-
+        X : array-like of shape (n_queries, n_features)
+        
         Returns
         -------
         y : ndarray of shape (n_queries,) or (n_queries, n_outputs)
